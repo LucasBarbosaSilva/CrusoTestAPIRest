16f265b3f753604a6cd5516ee7bca4ad4bbe57e1
Lucas Silva <blucas.oficial@gmail.com>
2022-11-16 10:56:17 -0300

    Matchers Hamcrest

--- src/main/java/lucas/rest/OlaMundoTest.java
+++ src/main/java/lucas/rest/OlaMundoTest.java
@@ -1,7 +1,13 @@
 package lucas.rest;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -45,5 +51,31 @@ public class OlaMundoTest {
 			//Teste
 			//.assertThat() //Não faz nada, é só para legibilidade
 			.statusCode(200);
+	}
+	
+	@Test
+	public void devoConhecerMatchersHamcrest() {
+		assertThat("Maria", Matchers.is("Maria"));
+						//Atual    Esperado
+		assertThat(128, Matchers.is(128));
+		assertThat(128, Matchers.isA(Integer.class));
+		assertThat(128d, Matchers.isA(Double.class));
+		assertThat(128d, Matchers.greaterThan(120d));
+		assertThat(128d, Matchers.lessThan(130d));
+		
+		List<Integer> impares = Arrays.asList(1, 3, 5, 7, 9);
+		assertThat(impares, hasSize(5));
+		assertThat(impares, contains(1,3,5,7,9));
+		assertThat(impares, containsInAnyOrder(1,3,5,9, 7));
+		//assertThat(impares, containsInAnyOrder(1,3,5,9)); //False - Precisa conter todos os elementos da lista
+		assertThat(impares, hasItem(1));
+		assertThat(impares, hasItems(1, 3, 5));
+		assertThat("Maria", is(not("Joao")));
+		assertThat("Maria", not("Joao"));
+//		assertThat("Maria", not("Maria"));
+//		assertThat("João", anyOf(is(not("João")), is("Maria")));
+		assertThat("João", anyOf(is("João"), is("Maria")));
+		assertThat("João Maria", allOf(startsWith("João"), endsWith("Maria")));
+		assertThat("Joaquina", allOf(startsWith("Joa"), endsWith("ina"), containsString("qui")));
 	}
 }
