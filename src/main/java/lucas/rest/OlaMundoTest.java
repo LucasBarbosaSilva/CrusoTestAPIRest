ea1724fd01bb46e0864ec89439c77b94c1c3d97d
Lucas Silva <blucas.oficial@gmail.com>
2022-11-17 09:13:35 -0300

    validacao body

--- src/main/java/lucas/rest/OlaMundoTest.java
+++ src/main/java/lucas/rest/OlaMundoTest.java
@@ -78,4 +78,16 @@ public class OlaMundoTest {
 		assertThat("João Maria", allOf(startsWith("João"), endsWith("Maria")));
 		assertThat("Joaquina", allOf(startsWith("Joa"), endsWith("ina"), containsString("qui")));
 	}
+	
+	@Test
+	public void devoValidarBody(){
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/ola")
+		.then()
+			.statusCode(200)
+			.body(is("Ola Mundo!"))
+			.body(containsString("Mundo"))
+			.body(is(notNullValue()));
+	}
 }
