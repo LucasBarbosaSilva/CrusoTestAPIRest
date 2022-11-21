613ae56a3afcd32b0b6832d755f7fea1186b58ea
Lucas Silva <blucas.oficial@gmail.com>
2022-11-21 08:49:11 -0300

    verificando Lista

--- src/main/java/lucas/rest/UserJsonTest.java
+++ src/main/java/lucas/rest/UserJsonTest.java
@@ -54,5 +54,21 @@ public class UserJsonTest {
 			;
 	}
 	
+	@Test
+	public void deveVerificarLista() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users/3")
+		.then()
+			.statusCode(200)
+			.body("name", containsString("Ana"))
+			.body("filhos", hasSize(2))
+			.body("filhos[0].name", is("Zezinho"))
+			.body("filhos[1].name", is("Luizinho"))
+			.body("filhos.name", hasItem("Luizinho"))
+			.body("filhos.name", hasItems("Luizinho", "Zezinho"))
+		;
+	}
+	
 	
 }
