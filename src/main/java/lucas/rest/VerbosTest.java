b646179a4d314bbd3a857a038b4f8eb447dfcd01
Lucas Silva <blucas.oficial@gmail.com>
2022-11-22 11:07:35 -0300

    alterando com put

--- src/main/java/lucas/rest/VerbosTest.java
+++ src/main/java/lucas/rest/VerbosTest.java
@@ -5,6 +5,8 @@ import static org.hamcrest.Matchers.*;
 
 import org.junit.Test;
 
+import io.restassured.http.ContentType;
+
 public class VerbosTest {
 	
 	@Test
@@ -27,16 +29,51 @@ public class VerbosTest {
 	@Test
 	public void naoDeveSalvarUsuarioSemNome() {
 		given()
-		.log().all()
-		.contentType("application/json")
-		.body("{\"age\": 50}")
-	.when()
-		.post("http://restapi.wcaquino.me/users") 
-	.then()
-		.log().all()
-		.statusCode(400)
-		.body("id", is(nullValue()))
-		.body("error", is("Name é um atributo obrigatório"))
-	;
+			.log().all()
+			.contentType("application/json")
+			.body("{\"age\": 50}")
+		.when()
+			.post("http://restapi.wcaquino.me/users") 
+		.then()
+			.log().all()
+			.statusCode(400)
+			.body("id", is(nullValue()))
+			.body("error", is("Name é um atributo obrigatório"))
+		;
+	}
+	
+	@Test
+	public void deveSalvarUsuarioViaXML() {
+		given()
+			.log().all()
+			.contentType(ContentType.XML)
+			.body("<user><name>Jose</name><age>50</age></user>")
+		.when()
+			.post("http://restapi.wcaquino.me/usersXML") 
+		.then()
+			.log().all()
+			.statusCode(201)
+			.body("user.@id", is(notNullValue()))
+			.body("user.name", is("Jose"))
+			.body("user.age", is("50"))
+		;
+	}
+	
+	@Test
+	public void deveAlterarUsuario() {
+		given()
+			.log().all()
+			.contentType("application/json")
+			.body("{\"name\": \"Claudio\", \"age\": 30}")
+		.when()
+			.put("http://restapi.wcaquino.me/users/1") 
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("id", is(1))
+			.body("name", is("Claudio"))
+			.body("age", is(30))
+			.body("salary", is(1234.5678f))
+		;
 	}
 }
