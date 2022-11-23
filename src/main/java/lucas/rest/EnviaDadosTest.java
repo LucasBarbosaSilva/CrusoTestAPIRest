7ceab9b40a3f32fe543347beb93afef7ef502518
Lucas Silva <blucas.oficial@gmail.com>
2022-11-23 10:22:21 -0300

    query via params

--- src/main/java/lucas/rest/EnviaDadosTest.java
+++ src/main/java/lucas/rest/EnviaDadosTest.java
@@ -1,6 +1,7 @@
 package lucas.rest;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.Test;
 
@@ -18,6 +19,22 @@ public class EnviaDadosTest {
 			.log().all()
 			.statusCode(200)
 			.contentType(ContentType.JSON)
+		;
+	}
+	
+	@Test
+	public void deveEnviarValorViaQueryViaParam() {
+		given()
+			.log().all()
+			.queryParam("format", "xml")
+			.queryParam("outra", "coisa")
+		.when()
+			.get("http://restapi.wcaquino.me/v2/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.XML)
+			.contentType(containsString("utf-8"))
 		;
 	}
 }
