ad79f65751589fb7e9ccd02a53e799b565ffdd59
Lucas Silva <blucas.oficial@gmail.com>
2022-11-24 11:52:44 -0300

    validar JSON

--- src/main/java/lucas/rest/SchemaTest.java
+++ src/main/java/lucas/rest/SchemaTest.java
@@ -6,6 +6,9 @@ import org.junit.Test;
 import org.xml.sax.SAXParseException;
 
 import io.restassured.matcher.RestAssuredMatchers;
+import io.restassured.module.jsv.JsonSchemaValidationException;
+import io.restassured.module.jsv.JsonSchemaValidator;
+
 
 public class SchemaTest {
 	@Test
@@ -33,4 +36,28 @@ public class SchemaTest {
 			.body(RestAssuredMatchers.matchesXsdInClasspath("users.xsd"))
 		;
 	}
+	
+	@Test
+	public void deveValidarSchemaJSON() {
+		given()
+			.log().all()
+		.when()
+			.get("https://restapi.wcaquino.me/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body(JsonSchemaValidator.matchesJsonSchema("users.json"))
+		;
+	}
 }
+//
+//io.restassured.module.jsv.JsonSchemaValidationException: com.fasterxml.jackson.core.JsonParseException: Unexpected character ('u' (code 117)): expected a valid value (number, String, array, object, 'true', 'false' or 'null')
+//at [Source: java.io.StringReader@2620e717; line: 1, column: 2]
+//
+//Caused by: com.fasterxml.jackson.core.JsonParseException: Unexpected character ('u' (code 117)): expected a valid value (number, String, array, object, 'true', 'false' or 'null')
+//at [Source: java.io.StringReader@2620e717; line: 1, column: 2]
+
+
+
+	
+	
