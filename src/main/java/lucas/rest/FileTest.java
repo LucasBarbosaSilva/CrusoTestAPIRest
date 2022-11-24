e57759edab93fd799e888fea08e16b41de1030a3
Lucas Silva <blucas.oficial@gmail.com>
2022-11-24 10:02:34 -0300

    envio arquivo

---
+++ src/main/java/lucas/rest/FileTest.java
@@ -0,0 +1,38 @@
+package lucas.rest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+import java.io.File;
+
+import org.junit.Test;
+
+
+public class FileTest {
+	@Test
+	public void deveObrigarEnvioArquivo() {
+		given()
+			.log().all()
+		.when()
+			.post("http://restapi.wcaquino.me/upload")
+		.then()
+			.log().all()
+			.statusCode(404)  //Deveria ser 400
+			.body("error", is("Arquivo não enviado"))
+		;
+	}
+	
+	@Test
+	public void deveFazerEnvioArquivo() {
+		given()
+			.log().all()
+			.multiPart("arquivo", new File("src/main/java/resources/pdf.pdf"))
+		.when()
+			.post("http://restapi.wcaquino.me/upload")
+		.then()
+			.log().all()
+			.statusCode(200)  
+			.body("name", is("pdf.pdf"))
+		;
+	}
+}
