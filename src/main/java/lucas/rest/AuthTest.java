d1d0601e8f92394eb8773dc6603ca9fd5680c366
Lucas Silva <blucas.oficial@gmail.com>
2022-11-28 10:50:27 -0300

    autenticacao basica

--- src/main/java/lucas/rest/AuthTest.java
+++ src/main/java/lucas/rest/AuthTest.java
@@ -20,4 +20,23 @@ public class AuthTest {
 			.body("name", is("Luke Skywalker"))
 		;
 	}
+	
+	@Test
+	public void deveObterClima() {
+		given()
+			.log().all()
+			.queryParam("q", "Arapiraca,AL,BR")
+			.queryParam("appid", "a002f898793c158b2b62e6ed543688ef")
+			.queryParam("unit", "metric")
+		.when()
+			.get("https://api.openweathermap.org/data/2.5/weather")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("name", is("Arapiraca"))
+			.body("coord.lon", is(-36.6611f))
+			.body("coord.lat", is(-9.7525f))
+		;
+	}
 }
+//a002f898793c158b2b62e6ed543688ef
