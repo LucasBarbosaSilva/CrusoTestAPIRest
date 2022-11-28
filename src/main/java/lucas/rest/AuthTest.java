2cdd8bb644f557bc1b404e9ca350484ea4465ff3
Lucas Silva <blucas.oficial@gmail.com>
2022-11-28 10:11:47 -0300

    api publica

---
+++ src/main/java/lucas/rest/AuthTest.java
@@ -0,0 +1,23 @@
+package lucas.rest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+
+
+public class AuthTest {
+	@Test
+	public void deveAcessarSWAPI() {
+		given()
+			.log().all()
+		.when()
+			.get("https://swapi.dev/api/people/1")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("name", is("Luke Skywalker"))
+		;
+	}
+}
