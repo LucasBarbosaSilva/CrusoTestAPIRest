d8e41c64425ef4369652b79eda91f576e4c1069c
Lucas Silva <blucas.oficial@gmail.com>
2022-11-28 12:14:45 -0300

    validacoes aplicacao real

---
+++ src/main/java/lucas/rest/CenarioReal.java
@@ -0,0 +1,66 @@
+package lucas.rest;
+
+import org.junit.Test;
+
+public class CenarioReal {
+	@Test
+	public void naoDeveAcessarAPISemToken() {
+		//get/contas
+	}
+	
+	@Test
+	public void deveIncluirContaComSucesso() {
+		//post/sigin
+		//post/contas
+	}
+	
+	@Test
+	public void deveAlterarContaComSucesso() {
+		//post/sigin
+		//put/contas/:id
+	}
+	
+	@Test
+	public void naodeveInlcuirContaComNomeRepetido() {
+		//post/sigin
+		//post/contas
+	}
+	
+	@Test
+	public void deveInlcuirMoviemntacaoComSucesso() {
+		//post/sigin
+		//post/transacoes 
+	}
+	
+	@Test
+	public void deveValidarCamposObrigatoriosMoviemntacao() {
+		//post/sigin
+		//post/transacoes 
+	}
+	
+	@Test
+	public void deveCadastrarTransacaoFutura() {
+		//post/sigin
+		//post/transacoes 
+	}
+	
+	@Test
+	public void naoDeveRemoverContaComMovimentacao() {
+		//post/sigin
+		//delete/contas/:id 
+	}
+	
+	@Test
+	public void deveCalcularSaldoContas() {
+		//post/sigin
+		//get/saldo
+	}
+	
+	@Test
+	public void deveRemoverMovimentacao() {
+		//post/sigin
+		//get/saldo
+	}
+	
+	
+}
