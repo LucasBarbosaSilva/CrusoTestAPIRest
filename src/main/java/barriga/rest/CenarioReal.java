12e3e44f82bf6a28d76f53e54d456dc49062eea3
Lucas Silva <blucas.oficial@gmail.com>
2022-12-01 21:20:40 -0300

    campos obrigatórios transacoes

--- src/main/java/barriga/rest/CenarioReal.java
+++ src/main/java/barriga/rest/CenarioReal.java
@@ -142,9 +142,26 @@ public class CenarioReal extends BaseTest{
 	}
 	
 	@Test
-	public void deveValidarCamposObrigatoriosMoviemntacao() {
-		//post/sigin
-		//post/transacoes 
+	public void deveValidarCamposObrigatoriosMoviemntacao() {				
+		given()
+			.header("Authorization", "JWT "+TOKEN)
+			.body("{}")
+		.when()
+			.post("/transacoes")
+		.then()
+			.statusCode(400)
+			.body("$", hasSize(8))
+			.body("msg", hasItems(
+							"Data da Movimentação é obrigatório",
+							"Data do pagamento é obrigatório",
+							"Descrição é obrigatório",
+							"Interessado é obrigatório",
+							"Valor é obrigatório",
+							"Valor deve ser um número",
+							"Conta é obrigatório",
+							"Situação é obrigatório"
+					))			
+		;
 	}
 	
 	@Test
